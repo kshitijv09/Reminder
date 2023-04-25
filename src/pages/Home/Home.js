e1afb617dce2677fb1d2b577240507036733a41b
kshitijv09 <kshitijv09@gmail.com>
2023-04-26 00:26:51 +0530

    Added Landing Page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,30 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
-import Animation from "../../components/Animation/Animation";
+import Landing from "../../components/Landing/Landing";
 export default function () {
   return (
     <div className="home-container">
-      {/* <Animation /> */}
-      <div className="heading">
-        <div className="rem">REMINDER APP</div>
-      </div>
-      <div className="text">
-        <p>Do you keep forgetting important thing on a daily basis?</p>
-        <p>Has this caused you to lose a lot of opportunites recently?</p>
-        <p>
-          This Reminder App is the solution to all your problems. Sign Up to
-          fiind out how
-        </p>
-      </div>
-      <div className="nav-links">
-        <Link to="/signup">
-          <div> Sign Up</div>
-        </Link>
-        <Link to="/login">
-          <div>Log In </div>
-        </Link>
-      </div>
+      <Landing />
     </div>
   );
 }
